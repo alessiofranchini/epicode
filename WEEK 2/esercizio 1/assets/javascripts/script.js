462afbd42fd9cb090bde7daa41b9c1dab7b0d52f
alessiofranchini <123971820+alessiofranchini@users.noreply.github.com>
2023-02-06 16:56:04 +0100

    Add files via upload

---
+++ WEEK 2/esercizio 1/assets/javascripts/script.js
@@ -0,0 +1,6 @@
+window.alert('criceto');
+window.alert('giraffa');
+window.alert('leone');
+document.write('<h3>Iniziamo la nostra pratica</h3>');
+document.write('<p>output scritto da javascript</p>');
+document.write('<p>pantera scritto da javascript</p>');
