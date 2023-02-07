042df818136942c86f88b7295944436ebb324801
alessiofranchini <123971820+alessiofranchini@users.noreply.github.com>
2023-02-07 17:12:21 +0100

    Add files via upload

---
+++ WEEK 2/esercizio 2/assets/JavaScript/script.js
@@ -0,0 +1,10 @@
+const concatena = document.getElementById("concatena");
+const concatena2 = document.getElementById("concatena2");
+
+let nome = "Aldo, ";
+var ruolo= "cliente, ";
+const numero = "3.14, ";
+concatena.innerHTML = nome + ruolo + numero ;
+
+var pippo = nome + ruolo + numero ;
+concatena2.innerHTML= pippo
